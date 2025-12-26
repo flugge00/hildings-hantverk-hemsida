fa0724e73dc01bc383e0aeb191052e740c25817c
Hugo Asplund <flugge00@gmail.com>
2025-12-26 22:17:35 +0100

    Style with images

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# ljus-sidan
+# Hildings Hantverk Hemsida
+
+Här är instruktioner på hur man installerar för att börja utvecka.
+
+Vi använder Eleventy framework
+
+Hostar via Github pages (Inte än)
+
+Domän namn?
+
+Jag kör med WSL2 men går såklart att använda annat.
+sudo apt-get update 
+sudo apt-install npm
+
+npm run start
--- src/assets/js/product-modal.js
+++ src/assets/js/product-modal.js
@@ -2,8 +2,8 @@ window.openModal = function (image, name, price, shortDesc, longDesc) {
   document.getElementById("modal-image").src = image;
   document.getElementById("modal-name").textContent = name;
   document.getElementById("modal-price").textContent = price;
-  document.getElementById("modal-description").textContent =
-    longDesc || shortDesc;
+  document.getElementById("modal-description").textContent = shortDesc;
+  document.getElementById("modal-long-description").textContent = longDesc;
 
   document.getElementById("product-modal").classList.add("is-open");
 };
